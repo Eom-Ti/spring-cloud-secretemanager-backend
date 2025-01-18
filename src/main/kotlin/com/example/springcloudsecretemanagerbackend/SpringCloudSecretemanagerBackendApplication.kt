3e7875be6ff46a3842153bcc50b3c131c54254a1
엄태권 <akfls221@gmail.com>
2025-01-18 17:03:17 +0900

    init project

---
+++ src/main/kotlin/com/example/springcloudsecretemanagerbackend/SpringCloudSecretemanagerBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springcloudsecretemanagerbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudSecretemanagerBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudSecretemanagerBackendApplication>(*args)
+}
